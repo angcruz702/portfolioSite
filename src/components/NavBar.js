490af3655ede4b96609950a819a951db145b1c6c
Your Name <you@example.com>
2023-02-25 06:51:29 -0800

    Halfway

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,32 +1,60 @@
-import { Container, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faFacebook, faTwitter, faInstagram, faLinkedin} from '@fortawesome/free-brands-svg-icons';
+import logo from '../assets/images/logo.svg';
+import banner from '../assets/images/banner-bg.png';
+import footerBanner from '../assets/images/footer-bg.png';
 
+export const NavBar = () => {
 
-export const NavBar() {
+  const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    }
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [])
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  }
+
   return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+      <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+        <Container>
+          <Navbar.Brand href="/">
+            <img src={logo} alt="Logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <span className="navbar-toggler-icon"></span>
+          </Navbar.Toggle>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+            </Nav>
+            <span className="navbar-text">
+              <div className="social-icon">
+                <a href="#"><FontAwesomeIcon icon ={faFacebook} className="fa" /></a>
+                <a href="#"><FontAwesomeIcon icon ={faLinkedin}/></a>
+                <a href="#"><FontAwesomeIcon icon ={faTwitter}/></a>
+              </div>
+                <button className="vvd"><span>Lets Connect</span></button>
+            </span>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    
+  )
 }
-
-export default BasicExample;
---
+++ src/components/Skills.js
@@ -0,0 +1,66 @@
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import {Container, Row, Col} from 'react-bootstrap';
+import meter1 from '../assets/images/meter1.svg';
+import meter2 from '../assets/images/meter2.svg';
+import meter3 from '../assets/images/meter3.svg';
+import colorSharp from '../assets/images/color-sharp.png';
+
+
+export const Skills =()=> {
+    const responsive = {
+        superLargeDesktop: {
+          // the naming can be any, depends on you.
+          breakpoint: { max: 4000, min: 3000 },
+          items: 5
+        },
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 3
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1
+        }
+      };
+
+      return(
+        <section className="skill" id="skills">
+            <Container>
+                <Row>
+                    <Col>
+                    <div className="skills-bx">
+                        <h2>
+                            Skills
+                        </h2>
+                        <p>My skills are these so far</p>
+                        <Carousel responsive={responsive} infinite={true} className="skill-slider">
+                            <div className="item">
+                                <img src={meter1} alt="Image"/>
+                                <h5>Wev Development</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter2} alt="Image"/>
+                                <h5>Brand Identity</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter3} alt="Image"/>
+                                <h5>Logo Design</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter1} alt="Image"/>
+                                <h5>Database/BackEnd</h5>
+                            </div>
+                        </Carousel>
+                    </div>
+                    </Col>
+                </Row>
+            </Container>
+            <img className="background-image-left" src={colorSharp}/>
+        </section>
+      )
+}
